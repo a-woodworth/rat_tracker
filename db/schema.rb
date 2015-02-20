369cf641c93f35123681d609f34487596d27fb7a
Woodworth <woodworthcommunications@gmail.com>
2015-02-20 15:08:49 -0600

    Add columns latitude and longitude to Sighting.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220160219) do
+ActiveRecord::Schema.define(version: 20150220210634) do
 
   create_table "sightings", force: :cascade do |t|
     t.string   "location"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20150220160219) do
     t.boolean  "public"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
