72a615f5b0cd646e4d81800eedce035722a41a75
Woodworth <woodworthcommunications@gmail.com>
2015-02-20 15:17:23 -0600

    Update validations in model to allow for reverse geocode to return address from coordinates.

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,9 @@
 class Sighting < ActiveRecord::Base
-  validates :location, :presence => true
-  validates :mobile, :format => { with: /\d{3}-\d{3}-\d{4}/, message: "Enter mobile as 999-999-9999"}
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
+
+#  validates :location, :presence => true
+
+#  validates :mobile, :format => { with: /\d{3}-\d{3}-\d{4}/, message: "Enter mobile as 999-999-9999"}
 end
