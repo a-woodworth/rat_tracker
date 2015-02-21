e6050ce5f314fd17f7e27aafc59415564de77958
Woodworth <woodworthcommunications@gmail.com>
2015-02-21 11:16:27 -0600

    Add seeds files. Place form_for in edit view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sightings_file = Rails.root.join('lib', 'seeds', '311rats.json').to_s
+sighting_hashes = JSON.parse(open(sightings_file).read)
+
+ActiveRecord::Base.transaction do
+
+  Sighting.destroy_all
+  sighting_hashes.each do |sighting_hash|
+    Sighting.create!(sighting_hash)
+  end
+
+  puts "There are now #{Sighting.count} rows in the sightings table"
+
+
+end
