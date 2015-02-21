18b706511b5f348c13ea1d0df566d2eadb444d6e
hannahbasil <hannahbasil@gmail.com>
2015-02-20 22:12:06 -0600

    updated home page

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,7 +3,7 @@ class Sighting < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode  # auto-fetch address
 
-#  validates :location, :presence => true
+  validates :location, :presence => true
 
-#  validates :mobile, :format => { with: /\d{3}-\d{3}-\d{4}/, message: "Enter mobile as 999-999-9999"}
+  validates :mobile, :format => { with: /\d{3}-\d{3}-\d{4}/, message: "Enter mobile as 999-999-9999"}
 end
